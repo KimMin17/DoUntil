de2d41d17b48b285fe17d64972e4b35e72c740f7
Kim Min <kimmin17@sogang.ac.kr>
2023-09-13 02:36:19 +0900

    update

--- src/main/java/com/km/dountil/DountilApplication.java
+++ src/main/java/com/km/dountil/DountilApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DountilApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DountilApplication.class, args);
 	}
